15e626abb4e2acbae3a9ee28aa7ac7fed493f5d9
hdudowicz <hdudowicz@gmail.com>
2020-10-23 17:05:42 +0100

    Rotated x axis labels

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plot
 from matplotlib import style
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
@@ -20,9 +20,11 @@ print(df)
 # Create new column with python datetime to plt graph
 df["Date"] = df["Timestamp"].values.astype(dtype='datetime64[s]')
 
-plt.plot_date(x=df["Date"], y=df["Close"], fmt="b")
-plt.title("Bitcoin price from the start of 2017")
-plt.ylabel("Price in $")
-plt.grid(True)
-plt.show()
+plot.plot_date(x=df["Date"], y=df["Close"], fmt="b")
+plot.title("Bitcoin closing price from the start of 2017")
+plot.ylabel("Price in $")
+plot.xlabel("Date")
+plot.xticks(rotation=40)
+plot.grid(True)
+plot.show()
 # var = data.groupby("Close")

13a71d0020c6d831ba5dcb299d21707fbee8cf06
hdudowicz <hdudowicz@gmail.com>
2020-10-23 17:02:22 +0100

    Added date column with date from timestamps. Plotted graph from dates and close values

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 from sklearn.svm import LinearSVC
 
+# Read CSV to pandas dataframe
 data = pandas.read_csv("bitstamp.csv")
 # Create new dataframe dropping rows with NaN values
 df = data.dropna()
@@ -15,4 +16,13 @@ timestamp = pandas.Timestamp("01/01/2017").timestamp()
 # Drop rows after 01/01/2017
 df = df[df["Timestamp"] > timestamp]
 print(df)
+
+# Create new column with python datetime to plt graph
+df["Date"] = df["Timestamp"].values.astype(dtype='datetime64[s]')
+
+plt.plot_date(x=df["Date"], y=df["Close"], fmt="b")
+plt.title("Bitcoin price from the start of 2017")
+plt.ylabel("Price in $")
+plt.grid(True)
+plt.show()
 # var = data.groupby("Close")

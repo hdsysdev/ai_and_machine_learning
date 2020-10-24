e6075f083e0c0a1c1b2267893e0d8e0b3ebac621
hdudowicz <hdudowicz@gmail.com>
2020-10-24 18:22:23 +0100

    Added knn and sgd regression

---
+++ kNN_regression.py
@@ -0,0 +1,52 @@
+import joblib
+import pandas
+import numpy as np
+import matplotlib.pyplot as plot
+from matplotlib import style
+from sklearn.datasets import make_classification
+from sklearn.linear_model import LinearRegression, SGDRegressor
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.svm import LinearSVC
+
+
+# Read CSV to pandas dataframe
+data = pandas.read_csv("bitstamp.csv")
+# Create new dataframe dropping rows with NaN values
+df = data.dropna()
+
+# Get unix timestamp for 01/01/2017
+timestamp = pandas.Timestamp("01/01/2017").timestamp()
+# Drop rows after 01/01/2017
+df = df[df["Timestamp"] > timestamp]
+
+# Create new column with python datetime to plt graph
+# df["Date"] = df["Timestamp"].values.astype(dtype='datetime64[s]')
+#
+# plot.plot_date(x=df["Date"], y=df["Close"], fmt="b")
+# plot.title("Bitcoin closing price from the start of 2017")
+# plot.ylabel("Closing Price in $")
+# plot.xlabel("Date")
+# plot.xticks(rotation=40)
+# plot.grid(True)
+# plot.show()
+
+
+x = pandas.DataFrame(df["Timestamp"])
+y = pandas.DataFrame(df["Close"])
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=0)
+minMaxScaler = MinMaxScaler()
+scaled_x_train = minMaxScaler.fit_transform(x_train)
+scaled_x_test = minMaxScaler.fit_transform(x_test)
+scaled_y_train = minMaxScaler.fit_transform(y_train)
+scaled_y_test = minMaxScaler.fit_transform(y_test)
+regressor = KNeighborsRegressor()
+model = GridSearchCV(regressor, {'n_neighbors': [2, 3, 4, 5]}, cv=5)
+model.fit(scaled_x_train, scaled_y_train)
+
+joblib.dump(model, "kNN.model")
+print(model.score(scaled_x_test, scaled_y_test))
--- main.py
+++ main.py
@@ -3,8 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plot
 from matplotlib import style
 from sklearn.datasets import make_classification
+from sklearn.linear_model import LinearRegression, SGDRegressor
+from sklearn.metrics import accuracy_score, classification_report
 from sklearn.model_selection import train_test_split
-from sklearn.svm import LinearSVC
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.svm import LinearSVC, SVR
 
 # Read CSV to pandas dataframe
 data = pandas.read_csv("bitstamp.csv")
@@ -15,16 +19,28 @@ df = data.dropna()
 timestamp = pandas.Timestamp("01/01/2017").timestamp()
 # Drop rows after 01/01/2017
 df = df[df["Timestamp"] > timestamp]
-print(df)
 
 # Create new column with python datetime to plt graph
-df["Date"] = df["Timestamp"].values.astype(dtype='datetime64[s]')
+# df["Date"] = df["Timestamp"].values.astype(dtype='datetime64[s]')
+#
+# plot.plot_date(x=df["Date"], y=df["Close"], fmt="b")
+# plot.title("Bitcoin closing price from the start of 2017")
+# plot.ylabel("Closing Price in $")
+# plot.xlabel("Date")
+# plot.xticks(rotation=40)
+# plot.grid(True)
+# plot.show()
 
-plot.plot_date(x=df["Date"], y=df["Close"], fmt="b")
-plot.title("Bitcoin closing price from the start of 2017")
-plot.ylabel("Price in $")
-plot.xlabel("Date")
-plot.xticks(rotation=40)
-plot.grid(True)
-plot.show()
-# var = data.groupby("Close")
+
+x = pandas.DataFrame(df["Timestamp"])
+y = pandas.DataFrame(df["Close"])
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=0)
+minMaxScaler = MinMaxScaler()
+scaled_x_train = minMaxScaler.fit_transform(x_train)
+scaled_x_test = minMaxScaler.fit_transform(x_test)
+scaled_y_train = minMaxScaler.fit_transform(y_train)
+scaled_y_test = minMaxScaler.fit_transform(y_test)
+regressor = SVR(kernel='rbf')
+regressor.fit(scaled_x_train, scaled_y_train.ravel())
+print(regressor.score(scaled_x_test, scaled_y_test))
